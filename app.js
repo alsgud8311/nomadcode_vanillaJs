7cc48899dac597a02da656d26cd6c5da284b89b8
minhyung <dup33333@hufs.ac.kr>
2023-02-28 23:39:24 +0900

    Changes to be committed: 	modified:   app.js 	modified:   index.html ver 4.6

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden"
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    const username = loginInput.value;
-    localStorage.setItem("username",username);
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    paintGreetings(savedUsername);
+}
